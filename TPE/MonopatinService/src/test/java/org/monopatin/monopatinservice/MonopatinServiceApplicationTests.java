30971fd40db423506409186e16e98a1482b76209
santi <monsterspeed02@gmail.com>
2024-11-09 18:54:01 -0300

    services

---
+++ TPE/MonopatinService/src/test/java/org/monopatin/monopatinservice/MonopatinServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.monopatin.monopatinservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonopatinServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- TPE/TarifaService/src/test/java/org/monopatin/tarifaservice/TarifaServiceApplicationTests.java
+++ TPE/TarifaService/src/test/java/org/monopatin/tarifaservice/TarifaServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.monopatin;
+package org.monopatin.tarifaservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MonopatinApplicationTests {
+class TarifaServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
