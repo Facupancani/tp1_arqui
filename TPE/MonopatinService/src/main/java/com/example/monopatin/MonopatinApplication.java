e1aaa55559df2cd1e5490d509e4b9c743558f651
santi <monsterspeed02@gmail.com>
2024-11-09 18:34:52 -0300

    add tpe

---
+++ TPE/MonopatinService/src/main/java/com/example/monopatin/MonopatinApplication.java
@@ -0,0 +1,13 @@
+package com.example.monopatin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonopatinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonopatinApplication.class, args);
+	}
+
+}
